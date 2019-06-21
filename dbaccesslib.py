c7f99165b138404824cd93d2a588b433e8eaf5a5
Koushik-ks <koushik.rjn@gmail.com>
2019-06-21 12:11:00 +0530

    Exception Handling 1

--- dbaccesslib.py
+++ dbaccesslib.py
@@ -10,14 +10,20 @@ def write_ontoDB(id_user,name):
 	conn = sqlite3.connect("newnewDB.db")
 	sqlquery = "Insert into Test_id Values(" + str(id_user) +",'"+ str(name) + "');"
 	print(sqlquery)
-	conn.execute(sqlquery)
-	conn.commit()
-	conn.close()
+	try:
+		conn.execute(sqlquery)
+		conn.commit()
+		conn.close()
+	except:
+		conn.close()
 def read_fromDB():
 	conn = sqlite3.connect("newnewDB.db")
 	sqlquery = "Select * from Test_id;"
-	values = conn.execute(sqlquery)
-	values = values.fetchall()
-	conn.commit()
-	conn.close()
+	try:
+		values = conn.execute(sqlquery)
+		values = values.fetchall()
+		conn.commit()
+		conn.close()
+	except:
+		conn.close()
 	return values
