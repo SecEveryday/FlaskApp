3fcef46e775c37fc3bf493c0943cacb37a8af48e
Koushik-ks <koushik.rjn@gmail.com>
2019-07-15 15:43:05 +0530

    Pymongo Uri Connect

--- dbaccesslibUserInfo.py
+++ dbaccesslibUserInfo.py
@@ -10,7 +10,7 @@ sys.stdout = log_file
 sys.stderr = log_file
 import pymongo
 uri = "mongodb://6824f101-0ee0-4-231-b9ee:sTZfIFjKnQC0CNWcSLj7WSlBgs3gsN9m49bImfnxNLxMtGzu6ETyXHQ8X7NlrsFm5sPW1qYjjLEM2FxBjcJm0Q==@6824f101-0ee0-4-231-b9ee.documents.azure.com:10255/?ssl=true&replicaSet=globaldb"
-client = pymongo.MongoClient()
+client = pymongo.MongoClient(uri)
 print("Obtained the client")
 mydb = client.test	
 def read_fromDB():
