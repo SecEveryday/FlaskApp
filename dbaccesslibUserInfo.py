cc7ede5c5a8d20eb04eb2df819429daa89225fc9
Koushik-ks <koushik.rjn@gmail.com>
2019-07-16 12:31:08 +0530

    Delete User Functionality

--- dbaccesslibUserInfo.py
+++ dbaccesslibUserInfo.py
@@ -14,18 +14,15 @@ client = pymongo.MongoClient(uri)
 print("Obtained the client")
 mydb = client.test	
 def read_fromDB():
-	return json.dumps(list(mydb.userInfo.find({},{'_id' : 0,'user_id':0})), default=json_util.default)
+	return json.dumps(list(mydb.userInfo.find({"userDeleted":False},{'_id' : 0,'user_id':0})), default=json_util.default)
 def read_fromDBSpecfic(jsonData):
-	return json.dumps(list(mydb.userInfo.find({'name':{'$regex':".*"+jsonData["name"]+".*",'$options':'i'}},{'_id' : 0,'user_id':0})),default=json_util.default)
+	return json.dumps(list(mydb.userInfo.find({'name':{'$regex':".*"+jsonData["name"]+".*",'$options':'i'},"userDeleted":False},{'_id' : 0,'user_id':0})),default=json_util.default)
 def add_usertoDB(jsonData):
-	mydb.userInfo.insert({'name':jsonData['name'],'department':jsonData['department'],'building':jsonData['building'],'division':jsonData['division'],'email':jsonData['emailaddress'],'floor':jsonData['floor'],'cubicle':jsonData['cubicle'],"user_id":jsonData["user_id"]})
+	mydb.userInfo.insert({'name':jsonData['name'],'department':jsonData['department'],'building':jsonData['building'],'division':jsonData['division'],'email':jsonData['emailaddress'],'floor':jsonData['floor'],'cubicle':jsonData['cubicle'],"user_id":jsonData["user_id"],"userDeleted":False})
 	print("Sucessfully added")
 	return json.dumps({"status": "Success","statusreason": "addSuccess"})
 def delete_userfromDB(jsonData):
-	a = mydb.userInfo.find_one({"name":jsonData["name"]})
-	newDbref = DBRef("mydb.userInfo",a["_id"])
-	mydb.userMailInfo.update({"otherdbref":newDbref},{"$set":{"userDeleted":True}})
-	mydb.userInfo.remove({"name":jsonData["name"]})
+	mydb.userInfo.update({"name":jsonData["name"]},{"$set":"userDeleted":True})
 	return json.dumps({"status": "Success","statusreason": "deleteSuccess"})
 #Production Level Testing code
 def clear_DB():
