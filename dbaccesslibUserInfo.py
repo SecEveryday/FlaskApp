dca9c8912379149dec512d4b10f6ca90b06b911e
Koushik <koushik.rjn@gmail.com>
2019-07-26 14:20:59 +0530

    DB Access 6 and Logging Mechanism 3

--- dbaccesslibUserInfo.py
+++ dbaccesslibUserInfo.py
@@ -17,34 +17,34 @@ logger.setLevel(logging.DEBUG)
 uri = "mongodb://56a41323-0ee0-4-231-b9ee:G9c804dv0gVufeDcqrJ0sTJklu0Nw3keGEZbITc5lqU8WZQUup4i9nr7OQf84s8XeLFs7quTKeyO7z6U9Y5g6A==@56a41323-0ee0-4-231-b9ee.documents.azure.com:10255/?ssl=true&replicaSet=globaldb"
 client = pymongo.MongoClient(uri)
 logger.debug("Obtained the client")
-mydb = client.test	
+mydb = client.test  
 def read_fromDB(jsonData):
-	num = int(jsonData['page'])
-	skips = 10 * (num - 1)
-	return json.dumps(list(mydb.userInfo.find({"userDeleted":False},{'_id' : 0,'user_id':0}).skip(skips).limit(10)), default=json_util.default)
+    num = int(jsonData['page'])
+    skips = 10 * (num - 1)
+    return json.dumps(list(mydb.userInfo.find({"userDeleted":False},{'_id' : 0,'user_id':0}).skip(skips).limit(10)), default=json_util.default)
 def read_fromDBSpecfic(jsonData):
     logger.debug("This is the JsonData")
     logger.debug(jsonData)
-	for item in jsonData:
-		foundUser = mydb.userInfo.find({'name':{'$regex':".*"+item+"\s.*",'$options':'i'},"userDeleted":False},{'_id' : 0,'user_id':0})
-		if(foundUser.count({'user_id':0}) >= 1):
-			return json.dumps(list(foundUser),default=json_util.default)
-	return json.dumps({},default=json_util.default)
+    for item in jsonData:
+        foundUser = mydb.userInfo.find({'name':{'$regex':".*"+item+"\s.*",'$options':'i'},"userDeleted":False},{'_id' : 0,'user_id':0})
+        if(foundUser.count({'user_id':0}) >= 1):
+            return json.dumps(list(foundUser),default=json_util.default)
+    return json.dumps({},default=json_util.default)
 def add_usertoDB(jsonData):
-	mydb.userInfo.insert({'name':jsonData['name'],'department':jsonData['department'],'building':jsonData['building'],'division':jsonData['division'],'email':jsonData['emailaddress'],'floor':jsonData['floor'],'cubicle':jsonData['cubicle'],"user_id":jsonData["user_id"],"userDeleted":False})
-	logger.debug("Sucessfully added")
-	return json.dumps({"status": "Success","statusreason": "addSuccess"})
+    mydb.userInfo.insert({'name':jsonData['name'],'department':jsonData['department'],'building':jsonData['building'],'division':jsonData['division'],'email':jsonData['emailaddress'],'floor':jsonData['floor'],'cubicle':jsonData['cubicle'],"user_id":jsonData["user_id"],"userDeleted":False})
+    logger.debug("Sucessfully added")
+    return json.dumps({"status": "Success","statusreason": "addSuccess"})
 def delete_userfromDB(jsonData):
-	founduser = mydb.userInfo.find_one({"userDeleted":False,"name":jsonData["name"]},{"_id":1})
-	mydb.userInfo.update_many({"_id":founduser["_id"],"user_id":1},{"$set":{"userDeleted":True}})
-	return json.dumps({"status": "Success","statusreason": "deleteSuccess"})
+    founduser = mydb.userInfo.find_one({"userDeleted":False,"name":jsonData["name"]},{"_id":1})
+    mydb.userInfo.update_many({"_id":founduser["_id"],"user_id":1},{"$set":{"userDeleted":True}})
+    return json.dumps({"status": "Success","statusreason": "deleteSuccess"})
 def update_user(jsonData):
-	founduser = mydb.userInfo.find_one({"userDeleted":False,"name":jsonData["name"]},{"_id":1})
-	mydb.userInfo.update_many({"_id":founduser["_id"],"user_id":1},{"$set":{'department':jsonData['department'],'building':jsonData['building'],'division':jsonData['division'],'email':jsonData['emailaddress'],'floor':jsonData['floor'],'cubicle':jsonData['cubicle']}})
-	return json.dumps({"status": "Success","statusreason": "updateSucess"})
-	#Production Level Testing code
+    founduser = mydb.userInfo.find_one({"userDeleted":False,"name":jsonData["name"]},{"_id":1})
+    mydb.userInfo.update_many({"_id":founduser["_id"],"user_id":1},{"$set":{'department':jsonData['department'],'building':jsonData['building'],'division':jsonData['division'],'email':jsonData['emailaddress'],'floor':jsonData['floor'],'cubicle':jsonData['cubicle']}})
+    return json.dumps({"status": "Success","statusreason": "updateSucess"})
+    #Production Level Testing code
 def clear_DB():
-	mydb.userInfo.drop()
-	mydb.userMailInfo.drop()
-	return json.dumps({"status": "Success","statusreason": "deleteSuccess"})
+    mydb.userInfo.drop()
+    mydb.userMailInfo.drop()
+    return json.dumps({"status": "Success","statusreason": "deleteSuccess"})
 #
