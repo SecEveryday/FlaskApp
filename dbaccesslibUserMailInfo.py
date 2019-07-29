4e810c24f3cabdb5b7adde8caad93b41642d23d4
Koushik <koushik.rjn@gmail.com>
2019-07-29 14:28:04 +0530

    qr code in server 12

--- dbaccesslibUserMailInfo.py
+++ dbaccesslibUserMailInfo.py
@@ -211,8 +211,8 @@ def generateqrcode(jsonData):
         ilocation=3
     else:
         ilocation=4
-    logger.debug(jsonData.building)
-    colorCode=jsonCode["building"][jsonData.building]["id"]+':'+jsonCode["building"][jsonData.building]["division"][jsonData.division]["id"]+':'+jsonCode["building"][jsonData.building]["division"][jsonData.division]["dept"][jsonData.department]["id"]+':'+jsonCode["building"][jsonData.building]["division"][jsonData.division]["dept"][jsonData.department]["floor"][jsonData.floor]+'F:'+ilocation+'L:'+dateTimeNow
+    logger.debug(json["building"])
+    colorCode=jsonCode["building"][jsonData["building"]]["id"]+':'+jsonCode["building"][jsonData["building"]]["division"][jsonData["division"]]["id"]+':'+jsonCode["building"][jsonData["building"]]["division"][jsonData["division"]]["dept"][jsonData["department"]]["id"]+':'+jsonCode["building"][jsonData["building"]]["division"][jsonData["division"]]["dept"][jsonData["department"]]["floor"][jsonData["floor"]]+'F:'+ilocation+'L:'+dateTimeNow
     logger.debug("ColorCode - "+colorCode)
     logger.debug("generateColorCode:: ColorCode value ="+colorCode)
     return colorCode;
