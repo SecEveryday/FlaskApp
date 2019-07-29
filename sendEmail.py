337d67e4cfb5302d89c2aeceed8435175137db83
Koushik <koushik.rjn@gmail.com>
2019-07-29 15:12:22 +0530

    Email 2

--- sendEmail.py
+++ sendEmail.py
@@ -45,4 +45,4 @@ def execute(emailAddress):
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
         server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, text)
+        server.sendmail(sender_email, emailAddress, text)
