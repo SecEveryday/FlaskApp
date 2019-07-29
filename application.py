65cc0e22a7ed8fb30309b1403ff6278554b7ef0b
Koushik <koushik.rjn@gmail.com>
2019-07-29 18:24:49 +0530

    Email 13

--- application.py
+++ application.py
@@ -63,9 +63,9 @@ def do_ocr():
     logger.debug("Hey reached Start of OCR")
     file = request.files['filename']
     logger.debug(file)
-    file.save(os.path.join("./uploads", "testocr.jpg"))
     import ocr as to
     ocredText = to.execute(file)
+    file.save(os.path.join("./uploads", "testocr.jpg"))
     logger.debug("Before Splitting:")
     logger.debug(ocredText)
     ocredText = ocredText.split()
