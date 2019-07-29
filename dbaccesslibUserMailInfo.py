216c0cbbf3711b8c3afb05020462de13033a0ed7
Koushik <koushik.rjn@gmail.com>
2019-07-29 17:10:21 +0530

    Email 7

--- dbaccesslibUserMailInfo.py
+++ dbaccesslibUserMailInfo.py
@@ -216,7 +216,7 @@ def generateqrcode(jsonData,file):
     logger.debug("ColorCode - "+colorCode)
     logger.debug("generateColorCode:: ColorCode value ="+colorCode)
     import sendEmail as se
-    se.execute(str(jsonData["email"]),file)
+    se.execute(str(jsonData["email"]),colorCode)
     return colorCode;
 def addEntry(jsonData):
 	a = mydb.userInfo.find_one({"name":jsonData["name"]})
--- sendEmail.py
+++ sendEmail.py
@@ -1,10 +1,10 @@
 import email, smtplib, ssl
-
+import os
 from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-def execute(emailAddress,file):
+def execute(emailAddress,qrcode):
     subject = "An email with attachment from Python"
     body = "This is an email with attachment sent from Python"
     sender_email = "koushik.rjn@gmail.com"
@@ -18,16 +18,16 @@ def execute(emailAddress,file):
 
     # Add body to email
     message.attach(MIMEText(body, "plain"))
-
-    filename = "testocr.jpg"  # In same directory as script
+    os.mkdir(qrcode)
+    file.save(os.path.join(qrcode, "testocr.jpg"))
+    filename = qrcode+"/testocr.jpg"  # In same directory as script
 
     # Open PDF file in binary mode
-   # with open(filename, "rb") as attachment:
+    with open(filename, "rb") as attachment:
         # Add file as application/octet-stream
         # Email client can usually download this automatically as attachment
-    with open(file, "rb") as attachment:    
         part = MIMEBase("application", "octet-stream")
-        part.set_payload(file.read())
+        part.set_payload(attachment.read())
 
     # Encode file in ASCII characters to send by email    
     encoders.encode_base64(part)
@@ -47,3 +47,5 @@ def execute(emailAddress,file):
     with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
         server.login(sender_email, password)
         server.sendmail(sender_email, emailAddress, text)
+    os.remove(filename)
+    os.rmdir(qrcode)
