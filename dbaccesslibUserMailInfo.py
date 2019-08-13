842f90c9ec3b28f36a0fa64a21d0ac60558ddc45
Koushik <koushik.rjn@gmail.com>
2019-08-13 15:17:36 +0530

    Extra 51

--- dbaccesslibUserMailInfo.py
+++ dbaccesslibUserMailInfo.py
@@ -253,7 +253,8 @@ def addEntry(jsonData,tags,autoThrashed):
     scan_date = str(scan_date.day) + str(scan_date.month) + str(scan_date.year)
     end_date = str(end_date.day) + str(end_date.month) + str(end_date.year)
     if( not autoThrashed and len(tags) >= 3):
-        mydb.mltable.insert({"code":jsonData["code"],"tags": tags,"status":"Keep","user_id":1,"otherdbref":newDbref})
+        #mydb.mltable.insert({"code":jsonData["code"],"tags": tags,"status":"Keep","user_id":1,"otherdbref":newDbref})  Actual Code
+        mydb.mltable.insert({"code":jsonData["code"],"tags": tags,"status":"Thrash ","user_id":1,"otherdbref":newDbref})#Test code to be removed
         end_date = scan_date
     mydb.userMailInfo.insert({"code":jsonData["code"],"scan_date":scan_date,"end_date":end_date,"otherdbref":newDbref,"userDeleted":False,"user_id":1})
     return json.dumps({"status": "Success","statusreason": "WriteToDBSuccess"})
