226147e945f853d19e0507de376bac0cb20decf6
Koushik <koushik.rjn@gmail.com>
2019-08-27 15:04:13 +0530

    MFP 37

--- sendEmail.py
+++ sendEmail.py
@@ -5,6 +5,7 @@ from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.MIMEImage import MIMEImage
 import logging 
 #Create and configure logger 
 logging.basicConfig(filename="server.log", 
