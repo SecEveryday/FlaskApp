057fcbc0db845c9075edd4d0e7d0c285d34f359a
Koushik <koushik.rjn@gmail.com>
2019-08-28 12:35:52 +0530

    MFP 57

--- dbaccesslibUserMailInfo.py
+++ dbaccesslibUserMailInfo.py
@@ -250,10 +250,10 @@ def generateqrcode(jsonData,filenameJPG,tags,fromMFP):
     buf = BytesIO()
     img.save(buf)
     image_stream = buf.getvalue()
-    #newImage = base64.b64encode(image_stream)
-    newImage = image_stream
-    logger.debug(newImage)
-    newjsonData = {"name":jsonData["name"],"code":colorCode,"email":jsonData["email"],"division":jsonData["division"],"department":json["department"],"floor":jsonData["floor"],"cubicle":jsonData["cubicle"],"building":jsonData["building"]}
+    newImage = base64.b64encode(image_stream)
+    #newImage = image_stream
+    #logger.debug(newImage)
+    newjsonData = {"name":jsonData["name"],"code":colorCode,"email":jsonData["email"],"division":jsonData["division"],"department":jsonData["department"],"floor":jsonData["floor"],"cubicle":jsonData["cubicle"],"building":jsonData["building"],"img":newImage}
     return addEntry(newjsonData,tags,autoThrashed);
 def addEntry(jsonData,tags,autoThrashed):
     a = mydb.userInfo.find_one({"name":jsonData["name"]})
