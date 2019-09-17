12f9771c6208e2882c9a725f19176d34b60f4196
Koushik <koushik.rjn@gmail.com>
2019-09-17 17:12:11 +0530

    Mail Address Change 3

--- sendEmail.py
+++ sendEmail.py
@@ -67,5 +67,5 @@ def execute(emailAddress,filenameJPG,qrcode,img,autoThrashed,fromMFP):
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
         server.login(sender_email, password)
-        server.sendmail(sender_email, emailAddress, text)
+        server.sendmail(sender_email, "Koushik.Sridhar@toshiba-tsip.com", text)
     os.remove(filename)
